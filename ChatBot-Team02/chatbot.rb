2f604f78141bcef9653aa01765fcf18103c78bc9
Kevin Risden <compuwizard123@gmail.com>
2011-04-28 17:52:26 -0400

    display list of games

--- ChatBot-Team02/chatbot.rb
+++ ChatBot-Team02/chatbot.rb
@@ -2,3 +2,9 @@ $stdout.sync = true
 puts 'What is your name?'
 name = gets.chomp
 puts "Hello, #{name}!"
+puts "[1] Twister"
+puts "[2] Hearts"
+puts "[3] Checkers"
+puts "[4] Trouble"
+puts "[5] Global Thermonuclear War"
+
