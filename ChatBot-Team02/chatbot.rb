5fcb8bf71107f20f1098239d98ac9b82a6126440
Kevin Risden <compuwizard123@gmail.com>
2011-04-28 17:57:53 -0400

    implmement selection

--- ChatBot-Team02/chatbot.rb
+++ ChatBot-Team02/chatbot.rb
@@ -7,4 +7,12 @@ puts "[2] Hearts"
 puts "[3] Checkers"
 puts "[4] Trouble"
 puts "[5] Global Thermonuclear War"
-
+sel = gets.chomp.to_i
+if 0 < sel and sel < 5
+  puts 'Wrong game choice'
+elsif sel == 5
+  puts 'BOOM!'
+else
+  puts 'Invalid game choice'
+end
+exit(0)
